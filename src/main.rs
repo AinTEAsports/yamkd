1622226f4f347a7fe59280821654e0c37ec1b030
AinTea <lucrafteur61@gmail.com>
2024-07-05 21:12:06 +0200

    found a new bug

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ mod executer;
 mod filenode;
 
 fn main() {
+    // NOTE: dir1/dir2/(file1,dir3/file2) doesn't let me create 'dir3/file2', solutions:
+    // - find a 'std::fs::create_dir_all' for file
+    // - split the filename by 'OUTER_SEPARATOR', create the dirs with 'std::fs::create_dir_all'
+    // and then cd into it to create the file
+
     let argv = std::env::args().collect::<Vec<String>>();
 
     if argv.len() == 1 { return; }
